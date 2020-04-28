109ac54d7acfabe314a67083c1d873b8c1e416c8
Luke Sun <lukesun@users.noreply.github.com>
2020-04-28 17:16:00 +0800

    Init upload

---
+++ access_control_system/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Access Control',
+    'description': 'Manage access control devices.',
+    'author': 'Anonymous',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/acs_menu.xml',
+        'views/acs_view.xml',
+         'views/templates.xml',
+    ],
+
+    'demo': [
+        'demo/demo.device.xml',
+    ],
+}
+
