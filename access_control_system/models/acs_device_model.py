7c7b9171d23ae695d976c8ebbf233b1dce1056cf
Luke Sun <lukesun@me.com>
2020-05-06 23:34:26 +0800

    model change

--- access_control_system/models/acs_device_model.py
+++ access_control_system/models/acs_device_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models,api
 
 class AcsLocker(models.Model):
     _name = 'acs.locker'
@@ -13,8 +13,8 @@ class AcsLocker(models.Model):
     locker_floor =  fields.Char(string="樓層")
     locker_vesion =  fields.Char(string="期數")
     devicegroup_name =  fields.Char(string="門禁群組", required=True)
-
-class AcsDevice(models.Model):
+ 
+ class AcsDevice(models.Model):
     _name = 'acs.device'
     _description = '卡機設定'
 
@@ -24,17 +24,20 @@ class AcsDevice(models.Model):
     device_type = fields.Char(string='型號', size=15)
     device_port = fields.Integer( string='卡機Port' )
     node_id = fields.Integer( string='卡機站號' )
-    device_owner = fields.Char(string='門市', size=8)
     device_location = fields.Char(string='樓層', size=8)
     active = fields.Boolean('連線否', default=True)
     devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
+    device_owner = fields.Many2one('hr.department','門市')
+    devicegroup = fields.Many2one('acs.devicegroup','門禁群組')
 
 class AcsDeviceGroup(models.Model):
     _name = 'acs.devicegroup'
     _description = '門禁群組設定'
+    _rec_name = 'devicegroup_name'
 
     devicegroup_id = fields.Char(string="群組編號", required=True)
     devicegroup_name = fields.Char(string="群組名稱", required=True)
+    device_ids = fields.One2many( 'acs.device', 'id', string="所屬卡機")
 
 class AcsCard(models.Model):
     _name = 'acs.card'
