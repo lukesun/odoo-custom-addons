57e95f4bbc6d1a539a933476f4a5bd144d3acd2b
Luke Sun <lukesun@me.com>
2020-06-01 23:31:08 +0800

    update model & view, fix contact search

--- access_control_system/controllers/controllers.py
+++ access_control_system/controllers/controllers.py
@@ -2,12 +2,15 @@
 import json
 import logging
 import requests
+import datetime
 
 from odoo import http
 
 _logger = logging.getLogger(__name__)
 
-def http_log_response(logid , msg):
+def http_log_response(msg):
+    t = datetime.datetime.now()
+    logid = t.strftime('%Y%m%d-%H%M-%S-%f')
     res = {
         "logid": logid,
         "desc": msg
@@ -16,19 +19,27 @@ def http_log_response(logid , msg):
 
 class AcsAPI(http.Controller):
     @http.route('/api/devices-async-result/',  auth='public', methods=["POST"], csrf=False)
-    def devices(self, **kw):
+    def async_result(self, **kw):
         _logger.warning( kw )
-        logid= kw.get('logid', False)
-        return http_log_response(logid, 'OK')
+        return http_log_response('OK')
 
     @http.route('/api/device-test-result/', auth='public', methods=["POST"], csrf=False)
-    def test(self, **kw):
+    def test_result(self, **kw):
         _logger.warning( kw )
-        logid= kw.get('logid', False)
-        return http_log_response(logid, 'OK')
+        return http_log_response('OK')
 
     @http.route('/api/device-record/', auth='public', methods=["POST"], csrf=False)
-    def object(self, **kw):
+    def device_reocrd(self, **kw):
         _logger.warning( kw )
-        logid= kw.get('logid', False)
-        return http_log_response(logid, 'OK')
+        return http_log_response('OK')
+    
+    @http.route('/api/devices-query', auth='public', methods=["POST"], csrf=False)
+    def devices_query(self, **kw):
+        _logger.warning( kw )
+        return http_log_response('OK')
+
+    @http.route('/api/devices-update', auth='public', methods=["POST"], csrf=False)
+    def device_update(self, **kw):
+        _logger.warning( kw )
+        return http_log_response('OK')
+
--- access_control_system/models/acs_device_model.py
+++ access_control_system/models/acs_device_model.py
@@ -7,6 +7,21 @@ from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationE
 
 import logging
 _logger = logging.getLogger(__name__)
+
+#class AcsCardOwner(models.Model):
+#    _name = 'acs.cardowner'
+#    _description = '卡片所有人'
+#    _inherit = 'res.partner'
+
+#    @api.model
+#    def name_search(self, name='', args=None, operator='ilike', limit=100):
+#        if self._context.get('search_by_vat', False):
+#            if name:
+#                args = args if args else []
+#                args.extend(['|', ['name', 'ilike', name], ['vat', 'ilike', name]])
+#                name = ''
+#        return super(AcsCardOwner, self).name_search(name=name, args=args, operator=operator, limit=limit)
+
 class AcsLocker(models.Model):
     _name = 'acs.locker'
     _description = '櫃位設定'
@@ -37,8 +52,6 @@ class AcsDevice(models.Model):
     device_port = fields.Char( string='卡機Port',size=4)
     node_id = fields.Char( string='卡機站號',size=3)
     device_location = fields.Char(string='樓層', size=8)
-    active = fields.Boolean('連線否', default=True)
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
 
     device_owner = fields.Many2one('hr.department',string='所屬門市(部門)',ondelete='set null')
 
@@ -57,7 +70,8 @@ class AcsDevice(models.Model):
                 }
             ]
         }
-        r = requests.post('http://odooerp.morespace.com.tw:9090/api/device-test',data=json.dumps(payload))
+        deviceserver=self.env['ir.config_parameter'].sudo().get_param('acs.deviceserver')
+        r = requests.post(deviceserver+'/api/device-test',data=json.dumps(payload))
         _logger.warning('%s, %s, %s' % (logid,r.status_code, r._content))
         
         message = {
@@ -109,15 +123,15 @@ class AcsDeviceGroup(models.Model):
                     "event": "add",
                     "uid": c.card.card_id,
                     "display": c.card.card_owner.name,
-                    "pin": c.accesscode,
+                    "pin": c.card.card_pin,
                     "expire_start": "2020-05-01",
                     "expire_end": "2020-05-31"
                 }
                 device["card"].append(card)
 
             payload["device"].append(device)
-
-        r = requests.post('http://odooerp.morespace.com.tw:9090/api/devices-async',data=json.dumps(payload))
+        deviceserver=self.env['ir.config_parameter'].sudo().get_param('acs.deviceserver')
+        r = requests.post(deviceserver+'/api/devices-async',data=json.dumps(payload))
         _logger.warning('%s, %s' % (logid, json.dumps(payload) ) )
         _logger.warning('%s, %s, %s' % (logid,r.status_code, r._content))
         message = {
@@ -152,15 +166,15 @@ class AcsDeviceGroup(models.Model):
                     "event": "update",
                     "uid": c.card.card_id,
                     "display": c.card.card_owner.name,
-                    "pin": c.accesscode,
+                    "pin": c.card.card_pin,
                     "expire_start": "2020-05-01",
                     "expire_end": "2020-05-31"
                 }
                 device["card"].append(card)
 
             payload["device"].append(device)
-
-        r = requests.post('http://odooerp.morespace.com.tw:9090/api/devices-async',data=json.dumps(payload))
+        deviceserver=self.env['ir.config_parameter'].sudo().get_param('acs.deviceserver')
+        r = requests.post(deviceserver+'/api/devices-async',data=json.dumps(payload))
         _logger.warning('%s, %s' % (logid, json.dumps(payload) ) )
         _logger.warning('%s, %s, %s' % (logid,r.status_code, r._content))
         message = {
@@ -197,8 +211,8 @@ class AcsDeviceGroup(models.Model):
                 device["card"].append(card)
 
             payload["device"].append(device)
-
-        r = requests.post('http://odooerp.morespace.com.tw:9090/api/devices-async',data=json.dumps(payload))
+        deviceserver=self.env['ir.config_parameter'].sudo().get_param('acs.deviceserver')
+        r = requests.post( deviceserver + '/api/devices-async' ,data=json.dumps(payload))
         _logger.warning('%s, %s' % (logid, json.dumps(payload) ) )
         _logger.warning('%s, %s, %s' % (logid,r.status_code, r._content))
         message = {
@@ -217,35 +231,45 @@ class AcsCard(models.Model):
     _name = 'acs.card'
     _description = '卡片設定'
 
-    user_role = fields.Many2one( 'res.partner.category' , string="身份")
     card_owner = fields.Many2one( 'res.partner' , string="聯絡人")
-    user_id = fields.Char(string='I D',compute='_get_partner_code')
-    user_name = fields.Char(string='名稱',compute='_get_partner_name')
-    user_phone = fields.Char(string='電話',compute='_get_partner_phone')
+    user_role = fields.Char(string='身份',compute='_get_owner_role')
+    user_id = fields.Char(string='I D',compute='_get_owner_id')
+    user_name = fields.Char(string='名稱',compute='_get_owner_name')
+    user_phone = fields.Char(string='電話',compute='_get_owner_phone')
     card_id = fields.Char(string='卡片號碼', required=True)
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
-
+    card_pin = fields.Char(string='卡片密碼')
     contract_ids = fields.One2many('acs.contract', 'card', string="合約清單")
 
-    def _get_partner_code(self):
+    def _get_owner_role(self):
+        for record in self:
+            record_role = '未知'
+            #if record.card_owner.customer_rank:
+            #    record_role = '客戶'
+            #if record.card_owner.supplier_rank:
+            #    record_role = '廠商'
+            record.user_role = record_role
+
+    def _get_owner_id(self):
         for record in self:
             record.user_id = record.card_owner.vat
 
-    def _get_partner_name(self):
+    def _get_owner_name(self):
         for record in self:
             record.user_name = record.card_owner.name
 
-    def _get_partner_phone(self):
+    def _get_owner_phone(self):
         for record in self:
             record.user_phone = record.card_owner.phone
 
 class AcsContract(models.Model):
     _name = 'acs.contract'
     _description = '合約設定'
-
-    contract_id = fields.Char(string="合約編號", required=True)
+    _sql_constraints = [
+        ('contract_id', 'unique(contract_id)', '合約編號不能重複！')
+        ]
+    contract_id = fields.Char(string="合約編號", required=True) 
+    
     contract_status = fields.Char(string='狀態')
-    accesscode =  fields.Char(string="通關密碼")
 
     card = fields.Many2one('acs.card',string='所屬卡片',ondelete='set null')
 
@@ -262,25 +286,44 @@ class AcsContract(models.Model):
     #def write(self,values):
         #campus_write = super(Campus,self).write(values)
         #return campus_write
-    #def unlink(self,values):
-    #    campus_unlink = super(Campus,self).unlink()
-    #    return campus_unlink
     
     def unlink(self):
         self.write({'devicegroup': False})
         return True
-    
 
 class AcsDeviceAccesscode(models.Model):
     _name = 'acs.deviceaccesscode'
-    _description = '每日更新通關密碼'
+    _description = '群組通關密碼'
 
     devicegroup_id = fields.Char(string="通用群組", required=True)
     devicegroup_name = fields.Char(string="群組名稱", required=True)
     accesscode = fields.Char(string="通關密碼", required=True)
     start_time = fields.Datetime(string='有效日期', required=True)
     end_time = fields.Datetime(string='失效日期')
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
+
+class AcsServicelog(models.Model):
+    _name = 'acs.servicelog'
+    _description = '卡機通訊紀錄'
+    #devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
+    
+
+class AcsConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+    deviceserver = fields.Char(string='卡機伺服器網址')
+    def _onchange_deviceserver(self):
+        if not self.deviceserver:
+            self.deviceserver = "http://odooerp.morespace.com.tw:9090"
+
+    def get_values(self):
+        res = super(AcsConfigSettings, self).get_values()
+        res.update(
+            deviceserver=self.env['ir.config_parameter'].sudo().get_param('acs.deviceserver')
+        )
+        return res
+
+    def set_values(self):
+        super(AcsConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('acs.deviceserver', self.use_security_lead)
 
 class AcsCardlog(models.Model):
     _name = 'acs.cardlog'
@@ -296,7 +339,6 @@ class AcsCardlog(models.Model):
     user_name = fields.Char(string='名稱')
     cardlog_time = fields.Datetime(string='刷卡時間')
     cardlog_result = fields.Char(string='刷卡狀態')
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
 
 class AcsEmployeeCardlog(models.Model):
     _name = 'acs.employeecardlog'
@@ -310,7 +352,6 @@ class AcsEmployeeCardlog(models.Model):
     user_name = fields.Char(string='名稱')
     employeecardlog_type = fields.Char(string='刷卡狀態')
     employeecardlog_time = fields.Datetime(string='刷卡時間')
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
 
 class AcsCardSettinglog(models.Model):
     _name = 'acs.cardsettinglog'
@@ -324,7 +365,6 @@ class AcsCardSettinglog(models.Model):
     card_id = fields.Char(string='卡片號碼')
     cardsettinglog_time = fields.Datetime(string='變更時間')
     cardsettinglog_user = fields.Char(string='變更者')    
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
 
 class AcsAccessCodelog(models.Model):
     _name = 'acs.accesscodelog'
@@ -338,4 +378,3 @@ class AcsAccessCodelog(models.Model):
     create_time = fields.Datetime(string='變更時間')
     expire_time = fields.Datetime(string='有效時間')
     accesscode = fields.Char(string='通關密碼')    
-    devicelog_id =  fields.Char(string='卡機紀錄編號', size=16 )
