342ada60f1ed5066c5883522ab20da1001d981c3
lukesun <lukesun@me.com>
2020-06-20 15:23:20 +0800

    fix user role change & model relation issues

--- access_control_system/models/acs_card.py
+++ access_control_system/models/acs_card.py
@@ -17,20 +17,40 @@ class AcsCard(models.Model):
     _rec_name = 'uid'
     confirmUnlink = fields.Boolean(string='確認刪除', default=False)
 
-    status = fields.Selection([ ('啟用', '啟用'),('作廢', '作廢'),],'卡片狀態', default='啟用', required=True)
-    
     uid = fields.Char(string='卡片號碼', required=True)
     pin = fields.Char(string='卡片密碼')
+    status = fields.Selection([ ('啟用', '啟用'),('作廢', '作廢'),],'卡片狀態', default='啟用', required=True)
 
-    owner_ref = fields.Reference( selection=[('res.partner', '客戶') , ('hr.employee', '員工'),], string='用戶', default=False)
-
-    partner_id = fields.Many2one('res.partner','客戶', default=False)
+    user_role = fields.Selection([ ('員工', '員工'),('客戶', '客戶'),('廠商', '廠商'),],'身份', default='客戶', required=True)
+    partner_id = fields.Many2one('res.partner','客戶或廠商', default=False)
     employee_id = fields.Many2one('hr.employee','員工', default=False)
+    
+    user_code = fields.Char(string='I D')
+    user_name = fields.Char(string='名稱')
+    user_phone = fields.Char(string='電話')
 
-    user_code = fields.Char(string='I D',compute='_get_user_code')
-    user_name = fields.Char(string='名稱',compute='_get_owner_name')
-    user_phone = fields.Char(string='電話',compute='_get_owner_phone')
-    user_role = fields.Char(string='身份',compute='_get_owner_role')
+    @api.onchange('user_role')
+    def _change_user_role(self):
+        for record in self:
+            if record.partner_id or record.employee_id:
+                raise ValidationError('已確認持有人的卡片無法更改持有人，請作廢或新增卡片。')
+
+
+    @api.onchange('partner_id')
+    def _update_partner_profile(self):
+        for record in self:
+            if record.partner_id:
+                record.user_name = record.partner_id.name
+                record.user_phone = record.partner_id.phone
+                record.user_code = record.partner_id.vat #統編 身份證
+
+    @api.onchange('employee_id')
+    def _update_employee_profile(self):
+        for record in self:
+            if record.employee_id:
+                record.user_name = record.employee_id.name
+                record.user_phone = record.employee_id.work_phone
+                record.user_code = record.employee_id.barcode #徽章 ID, 工號
 
     #卡片被授權進入的門禁群組
     devicegroup_ids = fields.Many2many(
@@ -49,6 +69,27 @@ class AcsCard(models.Model):
         column1='contract_id',
         column2='card_id',
     )
+
+#card ORM methods
+    @api.model
+    def create(self, vals):
+        write_card_log(self ,vals)
+        result = super(AcsCard, self).create(vals)
+        return result
+    
+    def write(self,vals):
+        write_card_log(self ,vals)
+        result = super(AcsCard, self).write(vals)
+        return result
+
+    def unlink(self):
+        if self.confirmUnlink:
+            write_card_log(self ,{})
+            result = super(AcsCard, self).unlink()
+            return result
+        else:
+            raise ValidationError("禁止未確認的刪除")
+
     # @api.constrains('devicegroup_ids')
     # def check_devicegroup_ids(self):
     #     for record in self:
@@ -77,79 +118,62 @@ class AcsCard(models.Model):
 
 #for compute fields
 #變更用戶欄位時顯示用戶類別
-    @api.onchange('owner_ref')
-    def _get_owner_role(self):
-        for record in self:
-            _logger.warning( '_get_owner_role in: %s' %( record.owner_ref ) )
-            if record.owner_ref:
-                if 'employee' in record.owner_ref:
-                    _logger.warning( 'employee! %s' %( record.owner_ref.employee ) )
-                    if (record.owner_ref.employee == False):
-                        #update partner_id
-                        #record.partner_id = self.env['res.partner'].sudo().search([['id','=',record.owner_ref.id ] ])
-                        record.partner_id = record.owner_ref.id
-                        if hasattr(record.owner_ref,'supplier_rank') and record.owner_ref.supplier_rank > 0 :
-                            _logger.warning( 'supplier_rank! %s' %( record.owner_ref.supplier_rank ) )
-                            record.user_role = '廠商'
-                        else:
-                            _logger.warning( 'Not supplier! %s' %( record.owner_ref.supplier_rank ) )
-                            record.user_role = '客戶'
-                else:
-                    #update employee_id
-                    #record.employee_id  = self.env['hr.employee'].sudo().search([['id','=',record.owner_ref.id ] ])
-                    record.employee_id = record.owner_ref.id
-                    record.user_role = '員工'
-            else:
-                #update partner_id & employee_id
-                record.partner_id = False
-                record.employee_id = False
-                record.user_role = ''
+    # @api.onchange('owner_ref')
+    # def _get_owner_role(self):
+    #     for record in self:
+    #         _logger.warning( '_get_owner_role in: %s' %( record.owner_ref ) )
+    #         if record.owner_ref:
+    #             if 'employee' in record.owner_ref:
+    #                 _logger.warning( 'employee! %s' %( record.owner_ref.employee ) )
+    #                 if (record.owner_ref.employee == False):
+    #                     #update partner_id
+    #                     #record.partner_id = self.env['res.partner'].sudo().search([['id','=',record.owner_ref.id ] ])
+    #                     record.partner_id = record.owner_ref.id
+    #                     if hasattr(record.owner_ref,'supplier_rank') and record.owner_ref.supplier_rank > 0 :
+    #                         _logger.warning( 'supplier_rank! %s' %( record.owner_ref.supplier_rank ) )
+    #                         record.user_role = '廠商'
+    #                     else:
+    #                         _logger.warning( 'Not supplier! %s' %( record.owner_ref.supplier_rank ) )
+    #                         record.user_role = '客戶'
+    #             else:
+    #                 #update employee_id
+    #                 #record.employee_id  = self.env['hr.employee'].sudo().search([['id','=',record.owner_ref.id ] ])
+    #                 record.employee_id = record.owner_ref.id
+    #                 record.user_role = '員工'
+    #         else:
+    #             #update partner_id & employee_id
+    #             record.partner_id = False
+    #             record.employee_id = False
+    #             record.user_role = ''
 
-    @api.onchange('owner_ref')
-    def _get_user_code(self):
-        for record in self:
-            if record.owner_ref:
-                if hasattr(record.owner_ref,'vat'):
-                    record.user_code = record.owner_ref.vat
-                else:
-                    record.user_code = ''
-            else:
-                record.user_code = ''
+    # @api.onchange('owner_ref')
+    # def _get_user_code(self):
+    #     for record in self:
+    #         if record.owner_ref:
+    #             if hasattr(record.owner_ref,'vat'):
+    #                 record.user_code = record.owner_ref.vat
+    #             else:
+    #                 record.user_code = ''
+    #         else:
+    #             record.user_code = ''
 
-    @api.onchange('owner_ref')
-    def _get_owner_name(self):
-        for record in self:
-            if record.owner_ref:
-                record.user_name = record.owner_ref.name
-            else:
-                record.user_name = ''
+    # @api.onchange('owner_ref')
+    # def _get_owner_name(self):
+    #     for record in self:
+    #         if record.owner_ref:
+    #             record.user_name = record.owner_ref.name
+    #         else:
+    #             record.user_name = ''
 
-    @api.onchange('owner_ref')
-    def _get_owner_phone(self):
-        for record in self:
-            if record.owner_ref:
-                if hasattr(record.owner_ref,'phone'):
-                    record.user_phone = record.owner_ref.phone
-                else:
-                    record.user_phone = ''
-            else:
-                record.user_phone = ''
-#card ORM methods
-    @api.model
-    def create(self, vals):
-        write_card_log(self ,vals)
-        result = super(AcsCard, self).create(vals)
-        return result
-    
-    def write(self,vals):
-        write_card_log(self ,vals)
-        result = super(AcsCard, self).write(vals)
-        return result
+    # @api.onchange('owner_ref')
+    # def _get_owner_phone(self):
+    #     for record in self:
+    #         if record.owner_ref:
+    #             if hasattr(record.owner_ref,'phone'):
+    #                 record.user_phone = record.owner_ref.phone
+    #             else:
+    #                 record.user_phone = ''
+    #         else:
+    #             record.user_phone = ''
 
-    def unlink(self):
-        if self.confirmUnlink:
-            write_card_log(self ,{})
-            result = super(AcsCard, self).unlink()
-            return result
-        else:
-            raise ValidationError("禁止未確認的刪除")
+
