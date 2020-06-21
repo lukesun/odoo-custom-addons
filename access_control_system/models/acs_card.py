5d84060b18079230e7945537045c03e21cb96139
lukesun <lukesun@me.com>
2020-06-22 01:18:53 +0800

    update card form UI logic

--- access_control_system/models/acs_card.py
+++ access_control_system/models/acs_card.py
@@ -22,19 +22,34 @@ class AcsCard(models.Model):
     status = fields.Selection([ ('新建', '新建'),('啟用', '啟用'),('作廢', '作廢'),],'卡片狀態', default='新建', required=True)
 
     user_role = fields.Selection([ ('員工', '員工'),('客戶', '客戶'),('廠商', '廠商'),],'身份', default='客戶', required=True)
-    partner_id = fields.Many2one('res.partner','外部用卡人', default=False)
+    partner_id = fields.Many2one('res.partner','持卡人', default=False)
     employee_id = fields.Many2one('hr.employee','員工', default=False)
     
     user_code = fields.Char(string='I D')
     user_name = fields.Char(string='名稱')
     user_phone = fields.Char(string='電話')
+    #卡片被授權進入的門禁群組
+    devicegroup_ids = fields.Many2many(
+        string='授權門禁群組',
+        comodel_name='acs.devicegroup',
+        relation='acs_devicegroup_acs_card_rel',
+        column1='devicegroup_id',
+        column2='card_id',
+    )
+    #卡片被授權的合約清單
+    contract_ids = fields.Many2many(
+        string='合約清單',
+        comodel_name='acs.contract',
+        relation='acs_contract_acs_card_rel',
+        column1='contract_id',
+        column2='card_id',
+    )
 
-    # _show_partner = fields.Boolean(string="All")
-    # _show_empolyee = fields.Boolean(string="All")
-    # _has_owner = fields.Boolean(string="All")
+    def action_create_contract(self):
+        raise UserError('Not support yet,action_create_contract.')
 
     def action_get_pin(self):
-        raise UserError('Not support yet,generate_pin.')
+        raise UserError('Not support yet,action_get_pin.')
         # for record in self:
         #     record.pin = "".join(choice(digits) for i in range(4))
 
@@ -42,6 +57,7 @@ class AcsCard(models.Model):
         raise UserError('Not support yet,action_uid_change.')
 
     def action_dispose(self):
+        raise UserError('Not support yet,action_dispose.')
         #text = """The case """+str(self.case_no)+""" will be forward to VC for further Approval. Are you want to proceed."""
         #query='delete from thesis_approval_message_oric'
         #self.env.cr.execute(query) 
@@ -60,7 +76,6 @@ class AcsCard(models.Model):
             }, 
             'res_id':self.id
         }
-        #raise UserError('Not support yet,action_dispose')
         
     @api.onchange('user_role')
     def _change_user_role(self):
@@ -76,11 +91,6 @@ class AcsCard(models.Model):
                     return {'domain': {'partner_id': [('supplier_rank', '=', 0)]}}
                 if record.user_role == '員工':
                     record.partner_id = False
-                
-    # @api.onchange('partner_id')
-    # def onchange_partner_id(self):
-    #     for rec in self:
-    #         return {'domain': {'order_id': [('partner_id', '=', rec.partner_id.id)]}}
 
     @api.onchange('partner_id')
     def _update_partner_profile(self):
@@ -97,24 +107,6 @@ class AcsCard(models.Model):
                 record.user_name = record.employee_id.name
                 record.user_phone = record.employee_id.work_phone
                 record.user_code = record.employee_id.barcode #徽章 ID, 工號
-
-    #卡片被授權進入的門禁群組
-    devicegroup_ids = fields.Many2many(
-        string='授權門禁群組',
-        comodel_name='acs.devicegroup',
-        relation='acs_devicegroup_acs_card_rel',
-        column1='devicegroup_id',
-        column2='card_id',
-    )
-
-    #卡片被授權的合約清單
-    contract_ids = fields.Many2many(
-        string='合約清單',
-        comodel_name='acs.contract',
-        relation='acs_contract_acs_card_rel',
-        column1='contract_id',
-        column2='card_id',
-    )
 
 #card ORM methods
     @api.model
@@ -164,65 +156,3 @@ class AcsCard(models.Model):
     #             for c in record.contract_ids:
     #                 c.unlink()
     #             raise ValidationError('只有客戶卡片才能更改合約列表')
-
-#for compute fields
-#變更用戶欄位時顯示用戶類別
-    # @api.onchange('owner_ref')
-    # def _get_owner_role(self):
-    #     for record in self:
-    #         _logger.warning( '_get_owner_role in: %s' %( record.owner_ref ) )
-    #         if record.owner_ref:
-    #             if 'employee' in record.owner_ref:
-    #                 _logger.warning( 'employee! %s' %( record.owner_ref.employee ) )
-    #                 if (record.owner_ref.employee == False):
-    #                     #update partner_id
-    #                     #record.partner_id = self.env['res.partner'].sudo().search([['id','=',record.owner_ref.id ] ])
-    #                     record.partner_id = record.owner_ref.id
-    #                     if hasattr(record.owner_ref,'supplier_rank') and record.owner_ref.supplier_rank > 0 :
-    #                         _logger.warning( 'supplier_rank! %s' %( record.owner_ref.supplier_rank ) )
-    #                         record.user_role = '廠商'
-    #                     else:
-    #                         _logger.warning( 'Not supplier! %s' %( record.owner_ref.supplier_rank ) )
-    #                         record.user_role = '客戶'
-    #             else:
-    #                 #update employee_id
-    #                 #record.employee_id  = self.env['hr.employee'].sudo().search([['id','=',record.owner_ref.id ] ])
-    #                 record.employee_id = record.owner_ref.id
-    #                 record.user_role = '員工'
-    #         else:
-    #             #update partner_id & employee_id
-    #             record.partner_id = False
-    #             record.employee_id = False
-    #             record.user_role = ''
-
-    # @api.onchange('owner_ref')
-    # def _get_user_code(self):
-    #     for record in self:
-    #         if record.owner_ref:
-    #             if hasattr(record.owner_ref,'vat'):
-    #                 record.user_code = record.owner_ref.vat
-    #             else:
-    #                 record.user_code = ''
-    #         else:
-    #             record.user_code = ''
-
-    # @api.onchange('owner_ref')
-    # def _get_owner_name(self):
-    #     for record in self:
-    #         if record.owner_ref:
-    #             record.user_name = record.owner_ref.name
-    #         else:
-    #             record.user_name = ''
-
-    # @api.onchange('owner_ref')
-    # def _get_owner_phone(self):
-    #     for record in self:
-    #         if record.owner_ref:
-    #             if hasattr(record.owner_ref,'phone'):
-    #                 record.user_phone = record.owner_ref.phone
-    #             else:
-    #                 record.user_phone = ''
-    #         else:
-    #             record.user_phone = ''
-
-
